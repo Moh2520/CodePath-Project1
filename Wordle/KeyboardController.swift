39d62438953a524b4224e9aa9a4a56a887c52af3
Moh2520 <mohamedibrahimma12@gmail.com>
2023-09-22 22:39:08 -0500

    Done with project

--- Wordle/KeyboardController.swift
+++ Wordle/KeyboardController.swift
@@ -30,8 +30,13 @@ class KeyboardController: NSObject,
     // Exercise 1: Return the correct number of items in a section
     // Tip: There's a helper method you can use located in this class
     // START YOUR CODE HERE
-    return 0
-    // END YOUR CODE HERE
+      if section >= 0 && section < keyboardRows.count {
+                    // Get the number of items in the specified section
+                    return keyboardRows[section].count
+                } else {
+                    return 0 // Return 0 for invalid sections
+                }
+      // END YOUR CODE HERE
   }
 
   func collectionView(_ collectionView: UICollectionView,
@@ -41,7 +46,7 @@ class KeyboardController: NSObject,
     cell.configure(with: keyboardRows[indexPath.section][indexPath.row])
     // Exercise 4: Pass in the `didSelectString` closure to the KeyboardCell's corresponding property
     // START YOUR CODE HERE
-    // ...
+      cell.didSelectString = didSelectString
     // END YOUR CODE HERE
     return cell
   }
